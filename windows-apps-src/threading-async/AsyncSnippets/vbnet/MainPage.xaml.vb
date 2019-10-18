e6fb801f6ba144794ace2a12209fe4b9568b4a10
Youssef Victor <31348972+Youssef1313@users.noreply.github.com>
2019-10-18 20:39:37 +0300

    Update MainPage.xaml.vb (#1968)

--- windows-apps-src/threading-async/AsyncSnippets/vbnet/MainPage.xaml.vb
+++ windows-apps-src/threading-async/AsyncSnippets/vbnet/MainPage.xaml.vb
@@ -10,23 +10,19 @@ Public NotInheritable Class MainPage
     ''' <summary>
     ''' Invoked when this page is about to be displayed in a Frame.
     ''' </summary>
-    ''' <param name="e">Event data that describes how this page was reached.  The Parameter
+    ''' <param name="e">Event data that describes how this page was reached. The Parameter
     ''' property is typically used to configure the page.</param>
     Protected Overrides Sub OnNavigatedTo(e As Navigation.NavigationEventArgs)
-
         Return
     End Sub
 
     '<SnippetDownloadRSS>
     ' Put the keyword Async on the declaration of the event handler.
     Private Async Sub Button_Click_1(sender As Object, e As RoutedEventArgs)
-
         Dim client As New Windows.Web.Syndication.SyndicationClient()
-
         Dim feedUri As New Uri("http://windowsteamblog.com/windows/b/windowsexperience/atom.aspx")
 
         Try
-
             Dim feed As SyndicationFeed = Await client.RetrieveFeedAsync(feedUri)
 
             '<SnippetCodeAfterAwait>
@@ -34,27 +30,18 @@ Public NotInheritable Class MainPage
             rssOutput.Text = feed.Title.Text & vbCrLf
 
             For Each item In feed.Items
-
-                rssOutput.Text += item.Title.Text & ", " &
-                                  item.PublishedDate.ToString() & vbCrLf
-
-            Next item
+                rssOutput.Text += $"{item.Title.Text}, {item.PublishedDate.ToString()}{vbCrLf}"
+            Next
             '</SnippetCodeAfterAwait>
 
-
         Catch ex As Exception
-
             ' Log Error.
             rssOutput.Text = "I'm sorry, but I couldn't load the page," &
                              " possibly due to network problems." &
                              "Here's the error message I received: " &
                               ex.ToString()
-
-
         End Try
 
     End Sub
     '</SnippetDownloadRSS>
-
-
 End Class
